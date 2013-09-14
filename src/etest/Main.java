5e83ab404858ac1e93f64a02c7d1a9f7d00891f7
Ramsharan Shrestha <ramsharan065@yahoo.com>
2013-09-14 10:01:42 +0545

    triggering the cloudbees next time

--- src/etest/Main.java
+++ src/etest/Main.java
@@ -23,5 +23,6 @@ public class Main {
 		p = Planet.MERCURY;
 		System.out.println(p.getmass()+"             "+p.getradius());
 		System.out.println("changed to trigger build in cloudbees");
+		System.out.println("triggering the cloudbees to build again");
 	}
 }
